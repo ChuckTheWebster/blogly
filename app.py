d59a1ea9d8d762bed76bf5fba1b21a92e0926b01
Charlotte Webster <charlotte.shae.webster@gmail.com>
2022-12-07 16:55:09 -0700

    add route/views: homepage, list_users, add_users_form, add_users)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
@@ -15,3 +15,39 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.get('/')
+def homepage():
+
+    return redirect('/users')
+
+@app.get('/users')
+def list_users():
+
+    users = User.query.all()
+
+    return render_template("user-list.html", users=users)
+
+
+@app.get('/users/new')
+def add_users_form():
+
+    return render_template('users-add.html')
+
+
+@app.post('/users/new')
+def add_users():
+
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form.get('image-url', None)
+
+    user = User(first_name, last_name, image_url)
+
+    db.session.add(user)
+    db.session.commit()
+
+    return render_template("")
+
+@app.get('/users/<int:user-id>')
+    def
