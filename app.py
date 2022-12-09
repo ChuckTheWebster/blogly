87e9e60c3f5769fc9cb0d128083d07416335d11a
Your Full Name <you@youremail.com>
2022-12-08 20:14:30 -0500

    writing tests2

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///blogly"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_ECHO"] = True
-# debug toolbar
+
+# app.debug = True
 app.config["SECRET_KEY"] = "oh-so-secret"
 debug = DebugToolbarExtension(app)
-
 
 connect_db(app)
 db.create_all()
